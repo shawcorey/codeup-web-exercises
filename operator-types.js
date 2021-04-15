aceb396da45e9d4d14e0dcdb3eb4bef9a9cbd2b4
shawcorey <shaw.corey@gmail.com>
2021-04-15 15:18:40 -0500

    operator class assignment

---
+++ operator-types.js
@@ -0,0 +1,38 @@
+"use strict"
+
+console.log('1' + 2)
+// answer is 12
+console.log(typeof '1' + 2)
+// answer is string2
+console.log(6 % 4)
+// answer is 2
+console.log('3 + 4 is ' + 3 + 4)
+// answer is 3 + 4 is 34
+console.log(0 < 0)
+// answer is false
+console.log('false' == false)
+// answer is false
+console.log(true == 'true')
+// answer is false
+console.log(5 >= -5)
+// answer is true
+console.log(!false || false)
+// answer is true
+console.log(true || "42")
+// answer is true
+console.log(!true && !false)
+// answer is false
+console.log(6 % 5)
+// answer is 1
+console.log(5 < 4 && 1 === 1)
+// answer is false
+console.log(typeof 'codeup' === 'string')
+// answer is true
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup')
+// answer is false
+console.log(4 >= 0 && 1 !== '1')
+// answer is true
+console.log(6 % 3 === 0)
+// answer is true
+console.log(5 % 2 !== 0)
+// answer is true
