d0aab65aa73aaf66c494c002e02d334933d7874d
shawcorey <shaw.corey@gmail.com>
2021-04-15 15:05:27 -0500

    added primitive class exercise

---
+++ primitive-types.js
@@ -0,0 +1,15 @@
+"use strict"
+console.log(99.9)
+// 99.9 is a number
+console.log("false")
+// "false" is a string
+console.log(false)
+// false is a boolean
+console.log("0")
+// "0" is a string
+console.log(0)
+// 0 is a number
+console.log(true)
+// true is a boolean
+console.log("true")
+// true is a string
