88531ecfe8c7910a364a3c3bfac9fc132df3e5d8
shawcorey <shaw.corey@gmail.com>
2021-04-16 11:21:55 -0500

    added functions exercise

---
+++ functions-exercise.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// MINI EXERCISE
+// TODO: Create functions which will return the product (*) of two numbers and the dividend (/) of two numbers, respectively
+
+
+
+
+
+
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+console.log(multiply(3, 3));
