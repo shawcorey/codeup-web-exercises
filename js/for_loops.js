dd23c755a4e5405c7824deaeeafb9fba7d32c32c
shawcorey <shaw.corey@gmail.com>
2021-04-23 15:28:36 -0500

    added for_loops.js exercises

---
+++ js/for_loops.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// function showMultiplicationChart(placeholder) {
+//
+//     for( var x = 1; x <= 10; x++){
+//
+//         console.log(placeholder + " x " + x + " = " +(placeholder * x));
+//     }
+//
+// }
+// showMultiplicationChart(7)
+
+
+//-------------------------------------------------------
+
+
+// for(var x = 0; x < 10; x++){
+//     var random = Math.floor(Math.random() * 20) + 180;
+//     if(random % 2 === 0){
+//         console.log(random + " is even");
+//     }else{
+//         console.log(random + " is odd");
+//     }
+//
+// }
+
+//------------------------------------------------------- Exercise 4
+
+//------------------------------------------------------- Exercise 5
+
+for (var x = 100; x >= 5; x -= 5 ){
+    console.log(x)
+}
