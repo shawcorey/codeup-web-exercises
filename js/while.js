973c3bcac2319a1178d765b41ab38e4af3f488a7
shawcorey <shaw.corey@gmail.com>
2021-04-23 12:34:36 -0500

    updated while loops exercise

--- js/while.js
+++ js/while.js
@@ -5,3 +5,8 @@ while (number < 65536) {
     number = number * number;
 
 }
+// // TODO: A parking lot with 30 parking spaces wants to display the amount of spaces available as it gets filled.
+//  Let's assume that there are allowing two cars into the parking lot at a time. As those cars take up spaces,
+//  write a do-while loop that prints the spaces available. If the parking lot gets filled, print "this parking lot
+//  is now full" to the console.
+// // HINT: think of a countdown, remember if statements can be used inside of your statements
