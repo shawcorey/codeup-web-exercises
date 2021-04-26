c0b76aaf82aff6408a00f4c91d51fdb34fd15085
shawcorey <shaw.corey@gmail.com>
2021-04-26 12:17:59 -0500

    updated iterating exercises

--- js/iterating.js
+++ js/iterating.js
@@ -6,18 +6,26 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
+var names = ["Corey", "Vivian", "Cody", "Blanka"];
 
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+function cyclingArray (names) {
+        for (let i = 0; i < names.length; i++) {
+            console.log(name)
+        }
+        }
+
+
 
     /**
      * TODO:
