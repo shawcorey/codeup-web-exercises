c07864470675a7d8e55dc96cdf2043a3021ded7b
shawcorey <shaw.corey@gmail.com>
2021-04-26 16:17:31 -0500

    updated array manipulations exercise

--- js/array-manipulation-lectures.js
+++ js/array-manipulation-lectures.js
@@ -11,8 +11,41 @@
     var days = ["Monday", "Tuesday", "Wednesday"];
     var missingDays = ["Thursday", "Friday", "Saturday", "Sunday"];
 
-    missingDays.forEach(function (day ){
-        days.push(days);
-    })
+    missingDays.forEach(function(day ){
+        days.push(day);
+    });
+    console.log(days);
+    console.log(missingDays);
 
-          })();
+
+
+// TODO - MINI EXERCISE
+//  -> Let's reverse engineer our days of the week array
+//  -> Make sure to console.log in order to see your code's behavior!
+//  -> Write a function which takes in your complete days of the week array
+//      -> It will eventually return your reverse-engineered array
+//  -> Using a FOR LOOP (not .forEach())
+//      -> pop() each item off the days array
+//      -> use the returned value from pop() to unshift the element
+//      -> on to the new array
+//  -> HINT: You may find that the loop doesn't behave as expected
+//      -> console.log EVERY possible thing which could change as your loop runs
+//          -> consider how the methods you are using change state of the array
+
+    console.log('This is the current array for days ' + days);
+    function daysOftheWeek(placeholder){
+        console.log(`${placeholder}`)
+        var newWeek = [];
+        var arrayLength = daysOftheWeek.length;
+        console.log(`Test parameters: ${placeholder}` )
+        for (let i = 0; i < arrayLength; i++){
+            newWeek.unshift( placeholder.pop() );
+        };
+        console.log(newWeek);
+    }
+    console.log(newWeek);
+
+    daysOftheWeek(days);
+
+
+})();
