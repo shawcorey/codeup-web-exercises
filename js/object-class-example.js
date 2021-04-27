cfae12006063b37b0cd1ae07f7533720e4bbf041
shawcorey <shaw.corey@gmail.com>
2021-04-27 15:34:38 -0500

    added zoo objects example exercise

--- js/object-class-example.js
+++ js/object-class-example.js
@@ -6,7 +6,10 @@ console.log(typeof thePistons);
 
 //
 var thePistons = {};
-console.log(typeof thePistons);
+//
+thePistons["city"] = "Detroit";
+thePistons["wins"] = -40;
+console.log(thePistons)
 //
 var thisisCorey = {
     name: "Corey",
@@ -14,4 +17,37 @@ var thisisCorey = {
     age: "37",
     yrborn: 1983,
 };
-console.log(thisisCorey)
+console.log(thisisCorey);
+//
+//
+// TODO: create an empty 'yourFaveSportsTeam' object using object literals
+// TODO: let's add a 'city', 'coach', and 'sport' property to our 'team' object using dot
+// TODO: let's add 'numOfPlayers', 'numOfChampsWon', and 'isBestTeam' properties to our same 'teams' object using bracket notation
+// TODO: write a function that returns the sport and numOfPlayers from your 'team' object
+var yourFaveSportsTeam = {};
+
+yourFaveSportsTeam.city = "Detroit"
+yourFaveSportsTeam.coach = "Dewayne Casey";
+yourFaveSportsTeam.sport = "Basketball";
+yourFaveSportsTeam.team = "Detroit Pistons";
+
+console.log(yourFaveSportsTeam);
+
+//
+yourFaveSportsTeam["numOfPlayers"] = 18;
+yourFaveSportsTeam["numOfChampsWon"] = 3;
+yourFaveSportsTeam["isBestTeam"] = true;
+
+console.log(yourFaveSportsTeam);
+
+function sport (){
+    return "numOfPlayers";
+
+}
+function teamData(obj){
+    let data1 = obj.sport;
+    let data2 = obj.numOfPlayers;
+
+}
+teamData(yourFaveSportsTeam)
+console.log(yourFaveSportsTeam);
---
+++ js/zoo-object-example.js
@@ -0,0 +1,22 @@
+"use strict";
+/** Mini Exercises */
+/*  TODO: Create a zoo object that contains basic info about the zoo (name, city,
+      isOpen, ranking, yearOpened), as
+      well as, information about at least 5 different animals (think nested objects). For each
+      Each animal must contain 3 identifiers (examples: hasFur, isMammal, animalSound, canFly,
+       livingPlace). Each animal must also contain a method that returns the sound of that
+        animal using the THIS keyword.
+       hint: Each animal should have at least 5 properties (one of them being animalSound);
+*/
+var zoo = {
+    name: "San Antonio Zoo",
+    city: "San Antonio",
+    isOpen: "12:00pm",
+    ranking: "5th",
+    yearOpened: 1963,
+    animal
+
+}
+console.log(zoo)
+
+//
