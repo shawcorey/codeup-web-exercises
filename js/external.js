227e3a4ea83ba80b72b3087aea3045752d7217fc
shawcorey <shaw.corey@gmail.com>
2021-05-08 14:47:43 -0500

    updated missing exercises

--- js/external.js
+++ js/external.js
@@ -3,6 +3,7 @@
 alert("Welcome to my Website!");
 prompt("What is your favorite color");
 var color = "blue";
-if (color = blue) {
+if (color = "blue") {
     color = "Me Too!";
 }
+console.log("Hello from external Javascript");
