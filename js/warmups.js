1f344eab86ce5036652c0656ecdcd272d6029a8a
shawcorey <shaw.corey@gmail.com>
2021-05-13 11:27:54 -0500

    created order-pizza page

--- js/warmups.js
+++ js/warmups.js
@@ -11,3 +11,21 @@ function remainder (number, divisor) {
     return number % divisor;
 }
 console.log(remainder(9,4))
+
+// 5/13/2021 Javascript warmup
+// Write a JavaScript program to construct the following pattern, using a nested for loop.
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+
+function showStars(str) {
+
+    for( var x = 1; x <= 5; x++){
+
+        console.log(placeholder + " x " + x + " = " +(placeholder * x));
+    }
+
+}
