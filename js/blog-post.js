956d56739d8ce87d995746750cc4a90c3026a008
shawcorey <shaw.corey@gmail.com>
2021-05-21 15:33:15 -0500

    finished blog exercise

--- js/blog-post.js
+++ js/blog-post.js
@@ -1,10 +1,37 @@
+const blogContainer = document.getElementById("blog-container");
+addButtonEvent();
 
 function addButtonEvent() {
+
     document.getElementById("submitBtn")
-        .addEventListener("click", addPost)
+        .addEventListener("click", addPost);
 }
 
 function addPost() {
 
+  blogContainer.appendChild(buildBlogPost());
 
+}
+
+function buildBlogPost() {
+
+    let blogPost = document.createElement("div");
+    blogPost.setAttribute("class","col-12 border mb-1");
+    blogPost.appendChild(getDateNode());
+    blogPost.appendChild(getBlogText());
+    return blogPost;
+}
+
+function getDateNode() {
+    let dateNode = document.createElement("p");
+    dateNode.textContent = new Date(Date.now()).toDateString();
+    return dateNode
+}
+
+function getBlogText() {
+    let blogText = document.getElementById("user-input").value;
+    let paragraphTag = document.createElement("p");
+    paragraphTag.textContent = blogText;
+    paragraphTag.setAttribute("class", "p-2");
+    return paragraphTag;
 }
