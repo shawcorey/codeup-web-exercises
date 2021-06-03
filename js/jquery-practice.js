7bf522176e505873469859c763652a528217efba
shawcorey <shaw.corey@gmail.com>
2021-06-03 14:40:56 -0500

    finished jquery event classroom exercise

--- js/jquery-practice.js
+++ js/jquery-practice.js
@@ -1,9 +1,26 @@
-$("h1").hide();
+// $("h1").hide();
+//
+// $("#pink").css("background-color","pink");
+//
+// $("#orange").css("background-color", "orange");
 
-$("#pink").css("background-color","pink");
+//
 
-$("#orange").css("background-color", "orange");
+$('#button').click(function() {
+    $("h1").css({"backgroundColor" : "blue"});
+});
 
+$( "p" ).dblclick(function() {
+    $(this).css("font-size", "18px");
+});
+
+let hoverIn = function () {
+    $(this).css("color","red")
+};
+let hoverOut = function () {
+    $(this).css("color","black")
+};
+$('p').hover(hoverIn, hoverOut);
 
 
 
